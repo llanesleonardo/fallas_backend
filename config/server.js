97e66a61dc660adcbfc0eb9ca1ed9ab3d1996b3c
Leonardo Llanes <llanesleonardo@gmail.com>
2021-07-02 18:48:29 -0500

    Backend dia 1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '083b1b65e5ff25f32ae031b07301b452'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '22981bee-919a-4abc-bd07-89e32b2d93bc'
+};
